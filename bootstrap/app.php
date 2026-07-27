64108322e173b444b1d67f020371289f745842f2
Dianingtyas Fazira <dianingtyasfazira400@gmail.com>
2026-07-28 04:51:25 +0700

    fix: trust Railway proxy for HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
